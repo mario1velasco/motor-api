604e38a3336de9b592f65dfb0cd4c5905685a507
Mario <mario@localhost.localdomain>
2020-02-20 11:54:20 +0100

    middleware

--- middleware/secure.middleware.js
+++ middleware/secure.middleware.js
@@ -1,8 +1,9 @@
 module.exports.isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log(`User authenticated`);
+    console.log(`User authenticated middleware`);
     next();
   } else {
+    console.log(`User NO authenticated middleware`);
     res.status(400).json({
       message: 'User is not authenticate'
     });
